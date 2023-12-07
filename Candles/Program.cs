ddc6fecfa7cb93353aa4bd7539e51beca210b48e
czirjekc@gmail.com <czirjekc@gmail.com>
2023-12-07 22:52:05 +0200

    New ones

---
+++ Candles/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int tallest = 0, candle_count = 0, candle_index, candle, length;
+        tallest = candles.Max();
+        length = candles.Count();
+
+        foreach (int elem in candles)
+        {
+            if (elem == tallest)
+                candle_count++;
+        }
+        /*        
+                while(candles.Count > 0)
+                {            
+                    candle = candles.Max();            
+                    candle_index = candles.IndexOf(candle);
+                    candles.RemoveAt(candle_index);
+                    if( candle == tallest)
+                        candle_count++;
+                    else
+                        return candle_count;
+                }
+        */
+        return candle_count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ MinMax/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace MinMax
+{
+
+
+    class Result
+    {
+
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void miniMaxSum(List<int> arr)
+        {
+            long maximum = 0, minimum = 0;
+            int max_index = 0, max_element = 0, min_element = 0, min_index = 0;
+            List<int> backup = new List<int>(arr);
+            int loops = 4;
+
+            // Get maximum first
+            for (int i = 0; i < loops; i++)
+            {
+
+                max_element = arr.Max();
+                max_index = arr.IndexOf(max_element);
+                maximum += max_element;
+                arr.RemoveAt(max_index);
+            }
+            //    Console.WriteLine("Maximum: {0}", maximum);
+            //        arr.Clear();
+            //        arr = backup.ToList();
+            // Now min      
+            for (int i = 0; i < loops; i++)
+            {
+
+                min_element = backup.Min();
+                min_index = backup.IndexOf(min_element);
+                minimum += min_element;
+                backup.RemoveAt(min_index);
+            }
+            Console.WriteLine("{0} {1}", minimum, maximum);
+        }
+
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.miniMaxSum(arr);
+        }
+    }
+
+}
